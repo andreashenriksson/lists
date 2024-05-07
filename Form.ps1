90cc8d14743272592a5f2fa0efa46c0723419f49
Andreas Henriksson <andreas.sp.henriksson@gmail.com>
2024-05-07 16:03:34 +0200

    Added Count button

--- Form.ps1
+++ Form.ps1
@@ -1,4 +1,3 @@
-﻿#Add-Type -assembly System.Windows.Forms
  [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 
 
@@ -19,7 +18,7 @@ function findDupes{
         }
     }
     $ja = $ja -join "`r`n" | Out-String
-    $outBox.Text = $ja
+    $outBox.Text = $ja.TrimEnd()
 }
 function findUniques {
     $cop=$inBox.Text;
@@ -39,7 +38,7 @@ function findUniques {
         }
     }
     $array = $array -join "`r`n" | Out-String
-    $outBox.Text = $array
+    $outBox.Text = $array.TrimEnd()
 }
 function toSQL{
     $cop=$inBox.Text;
@@ -50,7 +49,7 @@ function toSQL{
         $NewString = $NewString -replace ('\s+', ' ');
         $NewString = $NewString -replace (" ", "', '");
         $NewString = $NewString -join "`r`n" | Out-String
-        $outBox.Text = $NewString
+        $outBox.Text = $NewString.TrimEnd()
     }
     else{
         $cop = $cop -replace ('\s+', ' ');
@@ -59,26 +58,76 @@ function toSQL{
         if(!$cop.Contains(',')){ $cop = $cop -replace (" ", "', '"); }
         else{ $cop = $cop -replace (", ", "', '"); }
         $cop = $cop -join "`r`n" | Out-String
-        $outBox.Text = $cop
+        $outBox.Text = $cop.TrimEnd()
     }
 }
 
 function sortList{
-    $cop=$inBox.Text;
+    $cop = $inBox.Text;
     $cop = $cop -replace '(^\s+|\s+$)','' -replace '\s+',' '
     $cop = $cop.split(" ");
     $cop = $cop | Sort-Object
-    if ($asc = 0){
-        $cop = Reverse($cop)
+    if ($asc -eq $true){
+        if($cop -join "`r`n" -match '^[\d\s]*$'){
+            $cop = $cop | Sort-Object {[int]$_}
+        } else{
+            $cop = $cop | Sort-Object
+        }
+    }
+    else{
+        if($cop -join "`r`n" -match '^[\d\s]*$'){
+            $cop = $cop | Sort-Object {[int]$_} -Descending
+        } else{
+            $cop = $cop | Sort-Object -Descending
+        }
     }
     $cop = $cop -join "`r`n" | Out-String
-    $outBox.Text = $cop
+    $outBox.Text = $cop.TrimEnd()
 }
+
+function countInput{
+    $cop=$inBox.Text;
+    $cop = $cop -replace '(^\s+|\s+$)','' -replace '\s+',' '
+    $cop = $cop.split(" ");
+    $count = 0
+    foreach($obj in $cop){
+        $count = $count + 1
+    }
+
+    $outBox.Text = $count
+
+}
+
+
+function copyResult{
+    if($outbox.Text) {Set-Clipboard -Value $outBox.Text}
+}
+
+function pasteClip{
+    $inBox.Text = Get-Clipboard
+}
+
+function search{
+    $cop = $inBox.Text;
+    $cop = $cop -replace '(^\s+|\s+$)','' -replace '\s+',' '
+    $cop = $cop.split(" ");
+    $s   = $searchBox.Text; 
+    $res = New-Object System.Collections.ArrayList($null);
+
+    foreach($obj in $cop){
+        if($obj.Contains($s)){
+            [void]$res.Add($obj);
+        }
+    }
+    $res = $res -join "`r`n" | Out-String
+    $outBox.Text = $res.TrimEnd()
+}
+
 
 # Creating form
 $mainForm = New-Object System.Windows.Forms.Form 
-    $mainForm.Text = "Utils"
-    $mainForm.Size = New-Object System.Drawing.Size(600,600)
+    $mainForm.Text = "List tool"
+    $mainForm.Size = New-Object System.Drawing.Size(1000,600)
     $mainForm.AutoSize = $true
     $mainForm.FormBorderStyle = "FixedDialog"
     $mainForm.TopMost = $false
@@ -86,48 +135,44 @@ $mainForm = New-Object System.Windows.Forms.Form
     $mainForm.MinimizeBox = $true
     $mainForm.Font = New-Object System.Drawing.Font("Arial",10)
 
-# Creating a label (to be removed?)
-#$Label = New-Object System.Windows.Forms.Label
- #   $Label.Text = "Input:"
-  #  $Label.Location = New-Object System.Drawing.Point(10,10)
-   # $Label.AutoSize = $true
-    #$mainForm.controls.Add($Label)
 
 # Creating textboxes
-$inBox = New-Object System.Windows.Forms.TextBox
-    $inBox.Size = New-Object System.Drawing.Size(200,540)
+$inBox = New-Object System.Windows.Forms.RichTextBox
+    $inBox.Size = New-Object System.Drawing.Size(400,540)
     $inBox.Location = New-Object System.Drawing.Point(10,10)
     $inBox.Multiline = $true
     $inBox.ScrollBars = "Vertical"
     $inBox.Text = ""
     $mainForm.controls.Add($inBox)
 
-$outBox = New-Object System.Windows.Forms.TextBox
-    $outBox.Size = New-Object System.Drawing.Size(200,540)
-    $outBox.Location = New-Object System.Drawing.Point(390,10)
+$outBox = New-Object System.Windows.Forms.RichTextBox
+    $outBox.Size = New-Object System.Drawing.Size(400,540)
+    $outBox.Location = New-Object System.Drawing.Point(584,10)
     $outBox.Multiline = $true
     $outBox.ScrollBars = "Vertical"
     $mainForm.controls.Add($outBox)
 
-# Creating a button
+$searchBox = New-Object System.Windows.Forms.TextBox
+    $searchBox.Size = New-Object System.Drawing.Size(154,0)
+    $searchBox.Location = New-Object System.Drawing.Point(420,420)
+    $mainForm.controls.Add($searchBox)
+
+
+# Creating buttons
 $sortButton = New-Object System.Windows.Forms.Button
-    $sortButton.Location = New-Object System.Drawing.Size(275,150)
-    $sortButton.Size = New-Object System.Drawing.Size(60,25)
+    $sortButton.Location = New-Object System.Drawing.Size(464,50)
+    $sortButton.Size = New-Object System.Drawing.Size(66,25)
     $sortButton.Text = "Sort"
+    $mainForm.controls.Add($sortButton)
     $sortButton.Add_Click({
         sortList
-        if($asc=1){
-            $asc =0
-        }
-        else {
-            $asc = 1
-        }
+        $global:asc = !$asc;
     })
-    $mainForm.controls.Add($sortButton)
+
 
 $SQLButton = New-Object System.Windows.Forms.Button
-    $SQLButton.Location = New-Object System.Drawing.Size(275,200)
-    $SQLButton.Size = New-Object System.Drawing.Size(60,25)
+    $SQLButton.Location = New-Object System.Drawing.Size(464,100)
+    $SQLButton.Size = New-Object System.Drawing.Size(66,25)
     $SQLButton.Text = "SQL"
     $SQLButton.Add_Click({
         toSQL
@@ -135,8 +180,8 @@ $SQLButton = New-Object System.Windows.Forms.Button
     $mainForm.controls.Add($SQLButton)
 
 $duplicatesButton = New-Object System.Windows.Forms.Button
-    $duplicatesButton.Location = New-Object System.Drawing.Size(275,250)
-    $duplicatesButton.Size = New-Object System.Drawing.Size(60,25)
+    $duplicatesButton.Location = New-Object System.Drawing.Size(464,150)
+    $duplicatesButton.Size = New-Object System.Drawing.Size(66,25)
     $duplicatesButton.Text = "Dupes"
     $duplicatesButton.Add_Click({
         findDupes
@@ -144,15 +189,53 @@ $duplicatesButton = New-Object System.Windows.Forms.Button
     $mainForm.controls.Add($duplicatesButton)
 
 $uniqueButton = New-Object System.Windows.Forms.Button
-    $uniqueButton.Location = New-Object System.Drawing.Size(275,300)
-    $uniqueButton.Size = New-Object System.Drawing.Size(60,25)
+    $uniqueButton.Location = New-Object System.Drawing.Size(464,200)
+    $uniqueButton.Size = New-Object System.Drawing.Size(66,25)
     $uniqueButton.Text = "Uniques"
     $uniqueButton.Add_Click({
         findUniques
     })
     $mainForm.controls.Add($uniqueButton)
 
-[bool]$asc = 1;
+$countButton = New-Object System.Windows.Forms.Button
+    $countButton.Location = New-Object System.Drawing.Size(464,250)
+    $countButton.Size = New-Object System.Drawing.Size(66,25)
+    $countButton.Text = "Count"
+    $countButton.Add_Click({
+        countInput
+    })
+    $mainForm.controls.Add($countButton)
+
+$copyButton = New-Object System.Windows.Forms.Button
+    $copyButton.Location = New-Object System.Drawing.Size(514,525)
+    $copyButton.Size = New-Object System.Drawing.Size(66,25)
+    $copyButton.Text = "Copy"
+    $copyButton.Add_Click({
+        copyResult
+    })
+    $mainForm.controls.Add($copyButton)
+
+$pasteButton = New-Object System.Windows.Forms.Button
+    $pasteButton.Location = New-Object System.Drawing.Size(414,525)
+    $pasteButton.Size = New-Object System.Drawing.Size(66,25)
+    $pasteButton.Text = "Paste"
+    $pasteButton.Add_Click({
+        pasteClip
+    })
+    $mainForm.controls.Add($pasteButton)
+
+$searchButton = New-Object System.Windows.Forms.Button
+    $searchButton.Location = New-Object System.Drawing.Size(464,450)
+    $searchButton.Size = New-Object System.Drawing.Size(66,25)
+    $searchButton.Text = "Search"
+    $searchButton.Add_Click({
+        search
+    })
+    $mainForm.controls.Add($searchButton)
+
+
+
+$global:asc = $true;
 
 #$mainForm.ShowDialog()
 $mainForm.Add_Shown({$mainForm.Activate()})
